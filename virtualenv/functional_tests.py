5b82b776f434c2226ba879eeb0bcc9c1bef9312d
brian <brian_sjoberg@yahoo.com>
2019-06-28 09:27:37 -0400

    Switched to using hamcrest

--- virtualenv/functional_tests.py
+++ virtualenv/functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
+from hamcrest import equal_to, assert_that
 
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+assert_that(browser.title, equal_to('Welcome to Django'))
