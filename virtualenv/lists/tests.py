1d9c71e46db5adbe273ab652cfe76e5b5d52a0b6
brian <brian_sjoberg@yahoo.com>
2019-07-03 12:52:15 -0400

    Using render_to_string to make comparison against actual

--- virtualenv/lists/tests.py
+++ virtualenv/lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
@@ -14,6 +15,5 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
